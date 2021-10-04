34aa50f3e77b639e5b887fd6f934173fff65b481
MarioG8 <golikmariusz7@gmail.com>
2021-10-04 20:58:02 +0200

    finished the global styles component

--- components/Button.js
+++ components/Button.js
@@ -1,8 +1,30 @@
-import "../styles/Button.module.css";
+import styles from "../styles/Button.module.css";
+
+const STYLES = [
+  "btn__primary__solid",
+  "btn__warning__solid",
+  "btn__danger__solid",
+  "btn__success__solid",
+  "btn__primary__outline",
+  "btn__warning__outline",
+  "btn__danger__outline",
+  "btn__success__outline",
+];
+
+const SIZES = ["btn__medium", "btn__large"];
 
 function Button({ children, type, onClick, buttonStyle, buttonSize }) {
+  const checkButtonStyle = STYLES.includes(buttonStyle)
+    ? buttonStyle
+    : STYLES[0];
+  const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
+
   return (
-    <button onClick={onClick} type={type}>
+    <button
+      className={`btn ${checkButtonStyle} ${checkButtonSize}`}
+      onClick={onClick}
+      type={type}
+    >
       {children}
     </button>
   );
